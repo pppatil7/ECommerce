6e094339ae1bc5becaa205495092b9e15d162c1a
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-15 20:33:59 +0530

    create category controller

--- src/main/java/com/practice/controllers/CategoryController.java
+++ src/main/java/com/practice/controllers/CategoryController.java
@@ -1,4 +1,29 @@
 package com.practice.controllers;
 
+
+import com.practice.dto.CategoryDto;
+import com.practice.dto.CreateCategoryDto;
+import com.practice.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryDto> createCategory(@RequestBody CreateCategoryDto dto) {
+        CategoryDto categoryDto = categoryService.createCategory(dto);
+        return new ResponseEntity<>(categoryDto, HttpStatus.CREATED);
+    }
+
+
 }
