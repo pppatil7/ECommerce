933ca0d16596105ed2b81102197f216d5e9bb454
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-15 17:41:31 +0530

    product

---
+++ src/main/java/com/practice/entities/Product.java
@@ -0,0 +1,30 @@
+package com.practice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    private String productTitle;
+
+    private String productDescription;
+
+    private Double productPrice;
+
+
+}
