b0b32c37d8d9eb96f67ff56b9f63d71b4585b281
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-15 20:24:50 +0530

    create category

--- src/main/java/com/practice/services/CategoryService.java
+++ src/main/java/com/practice/services/CategoryService.java
@@ -1,4 +1,9 @@
 package com.practice.services;
 
+import com.practice.dto.CategoryDto;
+import com.practice.dto.CreateCategoryDto;
+
 public interface CategoryService {
+
+    CategoryDto createCategory(CreateCategoryDto dto);
 }
--- src/main/java/com/practice/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/practice/services/impl/CategoryServiceImpl.java
@@ -1,6 +1,27 @@
 package com.practice.services.impl;
 
+import com.practice.dto.CategoryDto;
+import com.practice.dto.CreateCategoryDto;
+import com.practice.entities.Category;
+import com.practice.repositories.CategoryRepository;
 import com.practice.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
+
+
+    private final CategoryRepository categoryRepository;
+
+    private final ModelMapper modelMapper;
+
+    @Override
+    public CategoryDto createCategory(CreateCategoryDto dto) {
+        Category category = modelMapper.map(dto, Category.class);
+        Category savedCategory = categoryRepository.save(category);
+        return modelMapper.map(savedCategory, CategoryDto.class);
+    }
 }
