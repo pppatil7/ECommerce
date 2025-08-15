01b3a582ab2742cabf6e2a7b1e5fcd2a31343185
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-15 19:52:31 +0530

    category dto

---
+++ src/main/java/com/practice/dto/CategoryDto.java
@@ -0,0 +1,17 @@
+package com.practice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CategoryDto {
+
+    private Long categoryId;
+
+    private String categoryName;
+}
