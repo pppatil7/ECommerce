a5a667e8fe04954b7b5ff7635fa24eecb359e691
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-15 17:55:40 +0530

    product repo

---
+++ src/main/java/com/practice/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.practice.repositories;
+
+import com.practice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
