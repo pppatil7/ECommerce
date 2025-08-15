8a7698789bff543b775565f4d64ecded5907c919
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-15 19:53:47 +0530

    category repo

---
+++ src/main/java/com/practice/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.practice.repositories;
+
+import com.practice.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
