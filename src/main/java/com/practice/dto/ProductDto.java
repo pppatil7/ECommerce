74569cca50a2bd1645cb34bbba7d4d65c6bfcd0a
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-15 17:46:15 +0530

    productdto

---
+++ src/main/java/com/practice/dto/ProductDto.java
@@ -0,0 +1,21 @@
+package com.practice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ProductDto {
+
+    private Long productId;
+
+    private String productTitle;
+
+    private String productDescription;
+
+    private Double productPrice;
+}
