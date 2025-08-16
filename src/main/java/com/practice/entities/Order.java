6c36945952d48e38cc155f8477322bdf0554c929
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-16 20:22:23 +0530

    order

---
+++ src/main/java/com/practice/entities/Order.java
@@ -0,0 +1,25 @@
+package com.practice.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    private Date orderDate;
+
+    private String orderStatus;
+
+    private Double orderTotalAmount;
+
+}
